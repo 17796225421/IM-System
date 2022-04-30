64b86795812cda2550ed95077444bfc0c1e4049a
zhouzihong <aqua5201314@outlook.com>
2022-04-30 07:05:24 -0700

    消息广播

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"sync"
 )
@@ -60,6 +61,28 @@ func (this *Server) Handler(conn net.Conn) {
 
 	// 广播当前用户上线消息
 	this.BroadCast(user, "已上线")
+
+	// 接收客户端发送的消息
+	go func() {
+		buf := make([]byte, 4096)
+		for {
+			n, err := conn.Read(buf)
+			if n == 0 {
+				this.BroadCast(user, "下线")
+				return
+			}
+
+			if err!=nil&&err!=io.EOF{
+				fmt.Println("Conn.Read err:",err)
+			}
+
+			// 提取用户消息，取出\n
+			msg:=string(buf[:n-1])
+
+			// 将得到的消息广播
+			this.BroadCast(user,msg)
+		}
+	}()
 
 	// 当前handler阻塞
 	select {}
