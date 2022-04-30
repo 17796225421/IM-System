f565f34a7231b791de7e22838d9334f399102fd7
zhouzihong <aqua5201314@outlook.com>
2022-04-30 06:19:11 -0700

    广播上线

--- server.go
+++ server.go
@@ -3,23 +3,66 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 type Server struct {
 	Ip   string
 	Port int
+
+	// 在线用户列表
+	OnlineMap map[string]*User
+	mapLock   sync.RWMutex
+
+	// 消息广播的channel
+	Message chan string
 }
 
 func NewServer(ip string, port int) *Server {
 	server := &Server{
-		Ip:   ip,
-		Port: port,
+		Ip:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*User),
+		Message:   make(chan string),
 	}
 	return server
 }
 
-func (this*Server)Handler(conn net.Conn){
-	fmt.Println("连接建立成功")
+// 监听Message广播消息channel的goroutine，一旦有消息就转发给所有在线的User
+func (this *Server) ListenMessage() {
+	for {
+		msg := <-this.Message
+
+		// 将msg发送给全部在线User
+		this.mapLock.Lock()
+		for _, cli := range this.OnlineMap {
+			cli.C <- msg
+		}
+		this.mapLock.Unlock()
+	}
+}
+
+// 广播消息
+func (this *Server) BroadCast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+	this.Message <- sendMsg
+}
+
+func (this *Server) Handler(conn net.Conn) {
+	// fmt.Println("连接建立成功")
+
+	// 用户上线，加入OnlineMap
+	user := NewUser(conn)
+
+	this.mapLock.Lock()
+	this.OnlineMap[user.Name] = user
+	this.mapLock.Unlock()
+
+	// 广播当前用户上线消息
+	this.BroadCast(user, "已上线")
+
+	// 当前handler阻塞
+	select {}
 }
 
 func (this *Server) Start() {
@@ -31,10 +74,13 @@ func (this *Server) Start() {
 
 	defer listener.Close()
 
+	// 启动监听Message的goroutine
+	go this.ListenMessage()
+
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			fmt.Println("listener.Accept err:",err)
+			fmt.Println("listener.Accept err:", err)
 			continue
 		}
 
---
+++ user.go
@@ -0,0 +1,34 @@
+package main
+
+import "net"
+
+type User struct {
+	Name string
+	Addr string
+	C    chan string
+	conn net.Conn
+}
+
+func NewUser(conn net.Conn) *User {
+	userAddr := conn.RemoteAddr().String()
+
+	user := &User{
+		Name: userAddr,
+		Addr: userAddr,
+		C:    make(chan string),
+		conn: conn,
+	}
+
+	go user.ListenMessage()
+
+	return user
+}
+
+// 监听当前User channel，一旦有消息，发送给对端客户端
+func (this *User) ListenMessage() {
+	for {
+		msg := <-this.C
+
+		this.conn.Write([]byte(msg+"\n"))
+	}
+}
