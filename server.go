be1cbf5a028d6d47a0bdc943837ec789264da64a
zhouzihong <aqua5201314@outlook.com>
2022-04-30 18:02:40 -0700

    超时踢出

--- server.go
+++ server.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"net"
 	"sync"
+	"time"
 )
 
 type Server struct {
@@ -19,6 +20,7 @@ type Server struct {
 	Message chan string
 }
 
+// 构造
 func NewServer(ip string, port int) *Server {
 	server := &Server{
 		Ip:        ip,
@@ -29,7 +31,7 @@ func NewServer(ip string, port int) *Server {
 	return server
 }
 
-// 监听Message广播消息channel的goroutine，一旦有消息就转发给所有在线的User
+// 监听server channel，广播给所有user
 func (this *Server) ListenMessage() {
 	for {
 		msg := <-this.Message
@@ -57,6 +59,8 @@ func (this *Server) Handler(conn net.Conn) {
 
 	user.Online()
 
+	isLive := make(chan bool)
+
 	// 接收客户端发送的消息
 	go func() {
 		buf := make([]byte, 4096)
@@ -71,18 +75,32 @@ func (this *Server) Handler(conn net.Conn) {
 				fmt.Println("Conn.Read err:", err)
 			}
 
-			// 提取用户消息，取出\n
+			// 提取用户消息，去除\n
 			msg := string(buf[:n-1])
 
 			// 将得到的消息广播
 			user.DoMessage(msg)
+
+			isLive <- true
 		}
 	}()
 
-	// 当前handler阻塞
-	select {}
+	for {
+		select {
+		// 用一个case来从channel取出数据，一旦取出数据，就说明活跃，本次select就结束，开始下一次for，也就是重置计时器
+		case <-isLive:
+		case <-time.After(time.Second * 10):
+			// 一个case来每10s触发一次超时踢出
+			user.SendMsg("长时间不活跃，你被踢了")
+
+			close(user.C)
+			conn.Close()
+			return
+		}
+	}
 }
 
+// 启动
 func (this *Server) Start() {
 	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", this.Ip, this.Port))
 	if err != nil {
@@ -92,7 +110,7 @@ func (this *Server) Start() {
 
 	defer listener.Close()
 
-	// 启动监听Message的goroutine
+	// 启动server的channel
 	go this.ListenMessage()
 
 	for {
@@ -102,6 +120,7 @@ func (this *Server) Start() {
 			continue
 		}
 
+		// 启动server对每个user的监听
 		go this.Handler(conn)
 	}
 }
--- user.go
+++ user.go
@@ -1,6 +1,9 @@
 package main
 
-import "net"
+import (
+	"net"
+	"strings"
+)
 
 type User struct {
 	Name string
@@ -43,16 +46,47 @@ func (this *User) Online() {
 func (this *User) Offline() {
 	// 用户下线，从OnlineMap删除
 	this.server.mapLock.Lock()
-	delete(this.server.OnlineMap,this.Name)
+	delete(this.server.OnlineMap, this.Name)
 	this.server.mapLock.Unlock()
 
 	// 广播当前用户下线消息
 	this.server.BroadCast(this, "下线")
 }
 
+func (this *User) SendMsg(msg string) {
+	this.conn.Write([]byte(msg))
+}
+
 // 用户处理消息的业务
 func (this *User) DoMessage(msg string) {
-	this.server.BroadCast(this,msg)
+	if msg == "who" {
+		// 查询当前在线用户
+		this.server.mapLock.Lock()
+		for _, user := range this.server.OnlineMap {
+			onlineMsg := "[" + user.Addr + "]" + user.Name + ":" + "在线...\n"
+			this.SendMsg(onlineMsg)
+		}
+		this.server.mapLock.Unlock()
+	} else if len(msg) > 7 && msg[:7] == "rename|" {
+		// 如果消息是rename|zhangsan，将当前客户端用户名改为zhangsan，并将修改成功消息发送给客户端用户
+		newName := strings.Split(msg, "|")[1]
+
+		_, ok := this.server.OnlineMap[newName]
+		if ok {
+			this.SendMsg("当前用户名被使用\n")
+		} else {
+			this.server.mapLock.Lock()
+			delete(this.server.OnlineMap, this.Name)
+			this.server.OnlineMap[newName] = this
+			this.server.mapLock.Unlock()
+
+			this.Name = newName
+			this.SendMsg("更新用户名为" + this.Name + "\n")
+		}
+	} else {
+		// 默认消息广播
+		this.server.BroadCast(this, msg)
+	}
 }
 
 // 监听当前User channel，一旦有消息，发送给对端客户端
